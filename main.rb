c68028abbc3f9f75d3883b9c5b1466a864f0c9ed
Adrian Wenceslao <awenceslao@student.neumont.edu>
2018-04-23 18:00:53 -0600

    Deleted story.rb and upload.rb because pointless

--- main.rb
+++ main.rb
@@ -1,1 +1,27 @@
+require "sinatra"
 
+get "/" do
+  erb :h
+end
+
+get "/:story" do
+  @name_of_story = params["story"]
+  erb :s
+end
+
+get "/output" do
+  erb :output
+end
+
+post "/:story" do
+  @name_of_story = params["story"]
+  @noun = params["noun_arr"]
+  @verb = params["verb_arr"]
+  @adjective = params["adjective_arr"]
+  erb :output
+end
+
+not_found do
+  status 404
+  halt 404, "Error 404, Page not found"
+end
