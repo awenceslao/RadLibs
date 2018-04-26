f2c5d535c97e2cff392131bd9c4d044d0539e5bc
Adrian Wenceslao <awenceslao@student.neumont.edu>
2018-04-25 19:17:00 -0600

    Fixed it

--- main.rb
+++ main.rb
@@ -17,19 +17,19 @@ get "/output" do
   erb :out
 end
 
+post "/upload" do
+  File.open("output/" + params["myfile"][:filename], "w") do |f|
+    f.write(params["myfile"][:tempfile].read)
+  end
+  erb :h
+end
+
 post "/:story" do
   @name_of_story = params["story"]
   @noun = params["noun_arr"]
   @verb = params["verb_arr"]
   @adjective = params["adjective_arr"]
   erb :out
-end
-
-post "/upload" do
-  File.open("uploads/" + params["myfile"][:filename], "w") do |f|
-    f.write(params["myfile"][:tempfile].read)
-  end
-  erb :h
 end
 
 not_found do
